2ae352b788b62fd908534ea098e41e93c0b6cda6
MaxMaes <tilburgsemax@gmail.com>
2023-12-12 07:55:53 +0100

    Created day 12 solution file
    Not solved

---
+++ src/main/kotlin/day12/Day12_part1.kt
@@ -0,0 +1,12 @@
+package day12
+
+import readInput
+
+fun main() {
+    val input = readInput("day_12_example")
+    input.map { line ->
+        val (springs, sequences) = line.split(" ").let { it[0] to it[1] }
+
+
+    }
+}
